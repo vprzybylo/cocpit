40d66e81538f51586aa5fa35eb3804da0ff91810
Vanessa Przybylo <vprzybylo@albany.edu>
2022-04-14 16:03:52 +0000

    add grid_shader plotting scripts/init

--- cocpit/plotting_scripts/__init__.py
+++ cocpit/plotting_scripts/__init__.py
@@ -1,4 +1,8 @@
-'''
+"""
+grid_shader:
+    - add alternating background color to matplotlib figure to distinguish classification report groups
+
+
 plot_metrics:
     - calculation and plotting functions for reporting performance metrics
     - confusion matrices
@@ -13,4 +17,12 @@ plot:
 saliency:
     - plots saliency maps of images to determine which pixels most contriute to the final output
     - called in notebooks/saliency_maps.py
-'''
+"""
+import glob
+from os.path import basename, dirname, isfile, join
+
+modules = glob.glob(join(dirname(__file__), "*.py"))
+__all__ = [
+    basename(f)[:-3] for f in modules if isfile(f) and not f.endswith("__init__.py")
+]
+from . import *  # noqa: F403 E402
