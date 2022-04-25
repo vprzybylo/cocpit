5b5ed8be26f6cd03fa53da054fa0b4612fb2c874
Vanessa Przybylo <vprzybylo@albany.edu>
2022-04-25 15:14:33 +0000

    timing class

--- cocpit/timing.py
+++ cocpit/timing.py
@@ -3,21 +3,18 @@ import time
 
 
 class Time:
-    def __init__(self, epoch, since_total, since_epoch):
-        self.epoch = epoch
-        self.since_total = since_total
+    def __init__(self, since_total, since_epoch):
+        self.since_total = since_total  # time.time() object
         self.since_epoch = since_epoch
 
-    def write_times(self, model):
+    def write_times(self, model_name: str, kfold: int = 0) -> None:
         """write time to train with respective modelname, epoch, and kfold to file"""
         time_elapsed = time.time() - self.since_total
-        with open(
-            "/data/data/saved_timings/model_timing_only_cpu.csv", "a", newline=""
-        ) as file:
+        with open("/data/data/saved_timings/model_timing.csv", "a", newline="") as file:
             writer = csv.writer(file)
-            writer.writerow([model_name, self.epoch, kfold, time_elapsed])
+            writer.writerow([model_name, kfold, time_elapsed])
 
-    def print_time_all_epochs(self):
+    def print_time_all_epochs(self) -> None:
         """print time it took for all epochs to train"""
         time_elapsed = time.time() - self.since_total
         print(
@@ -26,9 +23,9 @@ class Time:
             )
         )
 
-    def print_time_one_epoch(self, since_epoch):
+    def print_time_one_epoch(self) -> None:
         """print time it took for one epoch to complete"""
-        time_elapsed = time.time() - since_epoch
+        time_elapsed = time.time() - self.since_epoch
         print(
             "Epoch complete in {:.0f}m {:.0f}s".format(
                 time_elapsed // 60, time_elapsed % 60
