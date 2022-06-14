cce6d614e9646bdf6c74c8163ad73126c429b6fa
Vanessa Przybylo <vprzybylo@albany.edu>
2022-06-14 00:38:34 +0000

    new sphinx build for docs

--- cocpit/grad_cam_pytorch/main.py
+++ cocpit/grad_cam_pytorch/main.py
@@ -5,17 +5,13 @@ from torchvision import transforms
 import matplotlib.pyplot as plt
 from cocpit import config as config
 import numpy as np
+import matplotlib.cm as cm
 
 from cocpit.grad_cam_pytorch.grad_cam import (
     BackPropagation,
     Deconvnet,
     GradCAM,
     GuidedBackPropagation,
-)
-
-from cocpit.grad_cam_pytorch.save_images import (
-    save_gradient,
-    save_gradcam,
 )
 
 
@@ -53,15 +49,13 @@ class Runner:
         self.raw_image = raw_image
         self.image = image
         self.topk = topk
-        self.classes = config.CLASS_NAMES
-        self.output_dir = output_dir
         self.output_dir = output_dir
         self.target_layer = target_layer
         self.probs = None
         self.ids = None
 
     def deconv(self):
-        print("Deconvolution:")
+        """Deconvolution"""
         bp = BackPropagation(model=self.model)
         self.probs, self.ids = bp.forward(self.image)  # sorted
 
@@ -72,24 +66,17 @@ class Runner:
             deconv.backward(ids=self.ids[:, [i]])
             gradients = deconv.generate()
 
-            print(
-                "\t{} ({:.5f})".format(
-                    self.classes[self.ids.cpu().numpy()[0][i]],
-                    self.probs.cpu().detach().numpy()[0][i],
-                )
-            )
-            save_gradient(
+            self.save_gradient(
                 filename=os.path.join(
                     self.output_dir,
-                    f"deconvnet-{self.classes[self.ids.cpu().numpy()[0][i]]}.png",
+                    f"deconvnet-{config.CLASS_NAMES[self.ids.cpu().numpy()[0][i]]}.png",
                 ),
                 gradient=gradients,
             )
         deconv.remove_hook()
 
     def gradcam(self):
-        print("Grad-CAM/Guided Backpropagation/Guided Grad-CAM:")
-
+        """Grad-CAM"""
         gcam = GradCAM(model=self.model)
         _ = gcam.forward(self.image)
 
@@ -97,52 +84,40 @@ class Runner:
         _ = gbp.forward(self.image)
 
         for i in range(self.topk):
-            # Guided Backpropagation
             gbp.backward(ids=self.ids[:, [i]])
             gradients = gbp.generate()
 
-            # Grad-CAM
             gcam.backward(ids=self.ids[:, [i]])
             regions = gcam.generate(target_layer=self.target_layer)
 
-            print(
-                "\t{} ({:.5f})".format(
-                    self.classes[self.ids.cpu().numpy()[0][i]],
-                    self.probs.cpu().detach().numpy()[0][i],
-                )
-            )
-
-            # Guided Backpropagation
-            save_gradient(
+            self.save_gradient(
                 filename=os.path.join(
                     self.output_dir,
-                    f"-guided-{self.classes[self.ids.cpu().numpy()[0][i]]}.png",
+                    f"guided-{config.CLASS_NAMES[self.ids.cpu().numpy()[0][i]]}.png",
                 ),
                 gradient=gradients,
             )
 
-            # Grad-CAM
-            save_gradcam(
+            self.save_gradcam(
                 filename=os.path.join(
                     self.output_dir,
-                    f"-gradcam-{self.target_layer}-{self.classes[self.ids.cpu().numpy()[0][i]]}.png",
+                    f"gradcam-{self.target_layer}-{config.CLASS_NAMES[self.ids.cpu().numpy()[0][i]]}.png",
                 ),
                 gcam=regions,
                 raw_image=self.raw_image,
             )
 
             # Guided Grad-CAM
-            save_gradient(
+            self.save_gradient(
                 filename=os.path.join(
                     self.output_dir,
-                    f"-guided_gradcam-{self.target_layer}-{self.classes[self.ids[0][i].cpu().numpy()]}.png",
+                    f"guided_gradcam-{self.target_layer}-{config.CLASS_NAMES[self.ids[0][i].cpu().numpy()]}.png",
                 ),
                 gradient=torch.mul(regions.cpu(), gradients),
             )
 
     def vanilla_bp(self):
-        print("Vanilla Backpropagation:")
-
+        """Vanilla Backpropagation"""
         bp = BackPropagation(model=self.model)
         self.probs, self.ids = bp.forward(self.image)  # sorted
 
@@ -150,23 +125,42 @@ class Runner:
             bp.backward(ids=self.ids[:, [i]])
             gradients = bp.generate()
 
-            # Save results as image files
-            print(
-                "\t{} ({:.5f})".format(
-                    self.classes[self.ids[0][i].cpu().numpy()],
-                    self.probs.cpu().detach().numpy()[0][i],
-                )
-            )
-            save_gradient(
+            self.save_gradient(
                 filename=os.path.join(
                     self.output_dir,
-                    f"vanilla-{self.classes[self.ids[0][i].cpu().numpy()]}.png",
+                    f"vanilla-{config.CLASS_NAMES[self.ids[0][i].cpu().numpy()]}.png",
                 ),
                 gradient=gradients,
             )
 
         # Remove all the hook function in the "model"
         bp.remove_hook()
+
+    def save_gradient(self, filename, gradient):
+        gradient = gradient.cpu().numpy()[0].transpose(1, 2, 0)
+        gradient -= gradient.min()
+        gradient /= gradient.max()
+        gradient *= 255.0
+        cv2.imwrite(filename, np.uint8(gradient))
+        fig, (ax) = plt.subplots(
+            constrained_layout=True, figsize=(5, 5), ncols=1, nrows=1
+        )
+        ax.imshow(np.uint8(gradient))
+
+    def save_gradcam(self, filename, gcam, raw_image, paper_cmap=False):
+        gcam = gcam.cpu().numpy()[0]
+
+        cmap = cm.hot(gcam)[..., :3] * 255.0
+        if paper_cmap:
+            alpha = gcam[..., None]
+            gcam = alpha * cmap + (1 - alpha) * raw_image
+        else:
+            gcam = (cmap.astype(np.float) + raw_image.astype(np.float)) / 2
+        cv2.imwrite(filename, np.uint8(gcam[0]))
+        fig, (ax) = plt.subplots(
+            constrained_layout=True, figsize=(5, 5), ncols=1, nrows=1
+        )
+        ax.imshow(np.uint8(gcam[0]))
 
 
 def main(image_path, model, target_layer, topk=3, output_dir="./results"):
